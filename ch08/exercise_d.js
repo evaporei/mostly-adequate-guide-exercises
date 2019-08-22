2622947a7cb111d17b25eda04687d0e9fa1fcc42
Otávio Pace <otaviopp8@gmail.com>
2019-08-21 23:39:49 -0300

    ch08: solve exercise_d

--- ch08/exercise_d.js
+++ ch08/exercise_d.js
@@ -13,7 +13,22 @@
 // Remember either's two arguments must return the same type.
 
 // validateName :: User -> Either String ()
-const validateName = undefined;
+const validateName = ({ name }) =>
+  name.length > 3 ?
+    Either.of(null) :
+    left('user name should be bigger than 3')
+
+// saveAndWelcome :: User -> IO String 
+const saveAndWelcome = compose(
+  map(showWelcome),
+  save
+)
 
 // register :: User -> IO String
-const register = compose(undefined, validateUser(validateName));
+const register = compose(
+  either(
+    IO.of,
+    saveAndWelcome
+  ),
+  validateUser(validateName)
+);
