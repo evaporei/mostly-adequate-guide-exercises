acf5649762093241b31f9e1a5c60f1a904fe8881
Otávio Pace <otaviopp8@gmail.com>
2019-08-22 00:18:03 -0300

    ch10: solve exercise_b

--- ch10/exercise_b.js
+++ ch10/exercise_b.js
@@ -1,4 +1,6 @@
 // Rewrite `safeAdd` from exercise_a to use `liftA2` instead of `ap`.
 
 // safeAdd :: Maybe Number -> Maybe Number -> Maybe Number
-const safeAdd = undefined;
+const safeAdd = liftA2(add);
+
+// wooooooooooow lift is awesome!
