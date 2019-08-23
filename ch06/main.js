3c39a45b72f1801f187d4e110ccbde4e5f8cc808
Otávio Pace <otaviopp8@gmail.com>
2019-08-23 10:20:31 -0300

    ch06: solve exercise

--- ch06/main.js
+++ ch06/main.js
@@ -3,7 +3,7 @@ const ramda = CDN('ramda/0.21.0/ramda.min');
 const jquery = CDN('jquery/3.0.0-rc1/jquery.min');
 
 requirejs.config({ paths: { ramda, jquery } });
-require(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
+require(['jquery', 'ramda'], ($, { compose, curry, map, prop, path }) => {
   // -- Utils ----------------------------------------------------------
   const Impure = {
     trace: curry((tag, x) => { console.log(tag, x); return x; }), // eslint-disable-line no-console
@@ -12,19 +12,24 @@ require(['jquery', 'ramda'], ($, { compose, curry, map, prop }) => {
   };
 
   // -- Pure -----------------------------------------------------------
-  const host = 'api.flickr.com';
-  const path = '/services/feeds/photos_public.gne';
   const query = t => `?tags=${t}&format=json&jsoncallback=?`;
-  const url = t => `https://${host}${path}${query(t)}`;
+  const url = t => `https://api.flickr.com/services/feeds/photos_public.gne${query(t)}`;
+  const img = src => $('<img />', { src })
+  const getImagesOnJSON = compose(
+    map(img),
+    map(path(['media', 'm'])),
+    prop('items')
+  )
 
-  const img = src => $('<img />', { src });
-  const mediaUrl = compose(prop('m'), prop('media'));
-  const mediaUrls = compose(map(mediaUrl), prop('items'));
-  const images = compose(map(img), mediaUrls);
+  const render = compose(
+    Impure.setHtml('#js-main'),
+    getImagesOnJSON
+  )
 
-  // -- Impure ---------------------------------------------------------
-  const render = compose(Impure.setHtml('#js-main'), images);
-  const app = compose(Impure.getJSON(render), url);
+  const app = compose(
+    Impure.getJSON(render),
+    url
+  )
 
-  app('cats');
+  app('cats')
 });
